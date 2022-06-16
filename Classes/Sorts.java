37cc82d8ea1681213b6a43083c055b519653b409
6YJlaTuk <askarov_bulat@mail.ru>
2022-06-16 20:48:56 +0300

    Bubble Sort added

--- Classes/Sorts.java
+++ Classes/Sorts.java
@@ -71,4 +71,17 @@ public class Sorts {
         return arr;
     }
 
+    public int[] bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > -1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int x = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = x;
+                }
+            }
+        }
+        return arr;
+    }
+
 }
