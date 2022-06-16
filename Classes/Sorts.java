5ff429e6a3e71ee2707afa8dd08b939e846bc2bc
6YJlaTuk <askarov_bulat@mail.ru>
2022-06-16 20:38:59 +0300

    Selection Sort added

--- Classes/Sorts.java
+++ Classes/Sorts.java
@@ -58,4 +58,17 @@ public class Sorts {
         return arr;
     }
 
+    public int[] selectSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int x = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = x;
+                }
+            }
+        }
+        return arr;
+    }
+
 }
